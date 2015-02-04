f3259d71ec9e7122c5250c8ffd442cd5cb3cf311
Jip Spinnewijn <jip.spinnewijn@gmail.com>
2015-02-04 21:39:44 +0100

    Replaced tabs with spaces

--- AndroidModeType.hx
+++ AndroidModeType.hx
@@ -11,39 +11,39 @@ import openfl.utils.JNI;
  */
 class AndroidModeType
 {
-	/**
-	 * Declare constant for mode type checking according to Android API
-	 * 
-	 * https://developer.android.com/reference/android/content/res/Configuration.html#UI_MODE_TYPE_TELEVISION
-	 */
-	public static inline var UI_MODE_TYPE_TELEVISION = 4;
-	
-	/**
-	 * Retrieves the Mode Type from Java and checks if it is equal to Android TV
-	 * 
-	 * @return Boolean whether the app is running on Android TV
-	 */
-	public static function isAndriodTV():Bool
-	{
-		// Default is false
-		var result:Bool = false;
-		
-		// Only make the native call if running on Android
-		#if (android && openfl)
-		
-		var androidModeType:Int = get_android_mode_type();
-		if (androidModeType == UI_MODE_TYPE_TELEVISION) {
-			result = true;
-		}
-		
-		#end
-		
-		return result;
-	}
-	
-	#if (android && openfl)
-	
-	private static var get_android_mode_type = JNI.createStaticMethod("nl.spip.extension.AndroidModeType", "getAndroidModeType", "()I");
-	
-	#end
+    /**
+     * Declare constant for mode type checking according to Android API
+     * 
+     * https://developer.android.com/reference/android/content/res/Configuration.html#UI_MODE_TYPE_TELEVISION
+     */
+    public static inline var UI_MODE_TYPE_TELEVISION = 4;
+    
+    /**
+     * Retrieves the Mode Type from Java and checks if it is equal to Android TV
+     * 
+     * @return Boolean whether the app is running on Android TV
+     */
+    public static function isAndriodTV():Bool
+    {
+        // Default is false
+        var result:Bool = false;
+        
+        // Only make the native call if running on Android
+        #if (android && openfl)
+        
+        var androidModeType:Int = get_android_mode_type();
+        if (androidModeType == UI_MODE_TYPE_TELEVISION) {
+            result = true;
+        }
+        
+        #end
+        
+        return result;
+    }
+    
+    #if (android && openfl)
+    
+    private static var get_android_mode_type = JNI.createStaticMethod("nl.spip.extension.AndroidModeType", "getAndroidModeType", "()I");
+    
+    #end
 }
